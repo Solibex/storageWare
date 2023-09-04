48823b5b2fe5b78bfc7d2d26d64d45f5a4dfd6fe
yuiz <solibex.contact@gmail.com>
2023-09-05 02:43:09 +0700

    removed

--- code.lua
+++ code.lua
@@ -366,9 +366,6 @@ stuffbox:AddToggle('autopickup', {
 								if (root.Position - v:GetPivot().Position).Magnitude >= 20 then
                                     continue
                                 end
-                                newprint(
-									'Attempting to pick up '..v.Name..' at '..(root.Position - v:GetPivot().Position).Magnitude
-								)
                                 local prompt = v:FindFirstChild('ProximityPrompt', true) or v:FindFirstChild('Prompt', true)
                                 if prompt then
                                     fireproximityprompt(prompt)
