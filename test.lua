a7c48203dc52b8ee3258885e27ff0dbf890ded97
Solibex <92078408+Solibex@users.noreply.github.com>
2023-09-08 19:55:04 +0700

    didn't work

--- test.lua
+++ test.lua
@@ -1,19 +1,12 @@
-rconsolename('debug getgc table')
-writefile('testing.LOL', '')
 for i,v in getgc(false) do
-	if type(v) == 'function' and getfenv(v).script.Name == 'Client' and debug.getinfo(v).name == "isColliding" then
-		hookfunction(v, function() return false end)
+	if type(v) == 'function' and getfenv(v).script.Name == 'Utilities' and debug.getinfo(v).name == 'timeFormat' then
+		timeFormat = v
 	end
 end
 -- Krnl Monaco v2-- Krnl Monaco v2rconsolename('debug getgc table')
 local players = game:GetService("Players")
 local hud = players.LocalPlayer.PlayerGui:WaitForChild('HUD')
 local timeFormat
-for i,v in getgc(false) do
-	if type(v) == 'function' and getfenv(v).script.Name == 'Utilities' and debug.getinfo(v).name == 'timeFormat' then
-		timeFormat = v
-	end
-end
 workspace.ClockTime.Changed:Connect(function(value)
 	hud.Clock.Text = timeFormat(workspace.ClockTime.Value)
 end)
