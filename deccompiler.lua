f454762614f061c2dc1f8f8957fc30dbe516166b
yuiz <solibex.contact@gmail.com>
2023-09-16 01:20:16 +0700

    decompiler new script

--- deccompiler.lua
+++ deccompiler.lua
@@ -7,6 +7,7 @@ local v59 = require(Client).new(localplayer)
 v59:SetUp()
 local clientDecompiled = decompile(Client)
 local utlDecompiled = decompile(Utl)
+local effectsDecompiled = decompile(Utl:WaitForChild('Effects'))
 if isfile('stuff/client.txt') and readfile('stuff/client.txt') == clientDecompiled then
     warn('no new update | Client')
 else
@@ -16,6 +17,11 @@ if isfile('stuff/utl.txt') and readfile('stuff/utl.txt') == utlDecompiled then
     warn('no new update | Utl')
 else
     warn('new update! | Utl')
+end
+if isfile('stuff/effects.txt') and readfile('stuff/effects.txt') == utlDecompiled then
+    warn('no new update | Effects')
+else
+    warn('new update! | Effects')
 end
 writefile('stuff/client.txt', clientDecompiled)
 writefile('stuff/utl.txt', utlDecompiled)
