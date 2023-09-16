e6e96840d4cfaec131aacdb40e3dbd82baac1eec
yuiz <solibex.contact@gmail.com>
2023-09-16 22:51:22 +0700

    stuff

--- deccompiler.lua
+++ deccompiler.lua
@@ -2,7 +2,6 @@ repeat wait() until game:IsLoaded()
 local replicatedStorage = game:GetService("ReplicatedStorage")
 local Client, Utl = (replicatedStorage:WaitForChild("ClientSetup")):InvokeServer()
 local localplayer = game:GetService("Players").LocalPlayer
-shared.Utl = require(Utl)
 local v59 = require(Client).new(localplayer)
 v59:SetUp()
 local clientDecompiled = decompile(Client)
