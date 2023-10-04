aeeb0355a961b5df5818d6cfca4cdacc383d11b5
yuiz <solibex.contact@gmail.com>
2023-10-05 01:18:32 +0700

    empty functions removed

--- functionfinder.lua
+++ functionfinder.lua
@@ -1,7 +1,7 @@
 -- check workspace for functions!1
 writefile('testing.LOL', '')
 for i,v in getgc(false) do
-	if type(v) == 'function' and (getfenv(v).script.Name == 'Client' or getfenv(v).script.Name == 'Utilities') then
+	if type(v) == 'function' and (getfenv(v).script.Name == 'Client' or getfenv(v).script.Name == 'Utilities') and (debug.getinfo(v).name ~= '') then
 		appendfile('testing.LOL', debug.getinfo(v).name..' | '..getfenv(v).script.Name..'\n')
 	end
 end
