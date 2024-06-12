1fcdcf967f86463ac37da211b0437afd1c552609
yuiz <solibex.contact@gmail.com>
2024-06-12 20:49:33 +0700

    rewrite

--- rewrite.lua
+++ rewrite.lua
@@ -1,4 +1,31 @@
 local players = game:GetService('Players')
+local replicated_storage = game:GetService('ReplicatedStorage')
 
 local local_player = players.LocalPlayer
 
+local client_table, ult_table
+
+function notify(title, text, duration)
+	if client_table then
+        client_table:Noti({
+            Title = title or "none",
+            Text = text or "none", 
+            Duration = duration or 1
+        })
+    else
+        print(title, text, duration)
+    end
+end
+
+for _,v in getgc(true) do
+    if type(v) == 'table' and rawget(v, 'Player') then
+        client_table = v
+    end
+	if type(v) == 'table' and rawget(v, 'timeFormat') then
+		ult_table = v
+	end
+end
+
+if client_table and ult_table  then
+    
+end
--- test.lua
+++ test.lua
@@ -1,17 +1,23 @@
+local players = game:GetService("Players")
+
 repeat task.wait() until game:IsLoaded()
 local timeFormat = function(value)
 	return '[ timeformat fail ] - '..value
 end
-local players = game:GetService("Players")
-local hud = players.LocalPlayer:WaitForChild('PlayerGui'):WaitForChild('HUD')
+local local_player = players.LocalPlayer
+local player_gui = local_player.PlayerGui
+
+local hud = player_gui:WaitForChild('HUD')
 local clock_gui = hud:WaitForChild('Clock')
 clock_gui.Visible = true
 
 local clock_time = workspace:WaitForChild('ClockTime')
 
-for i,v in getgc(false) do
-	if type(v) == 'function' and getfenv(v).script.Name == 'Utilities' and debug.getinfo(v).name == 'timeFormat' then
-		timeFormat = v
+if getgc and #getgc(false) > 0 then
+	for i,v in getgc(false) do
+		if type(v) == 'function' and getfenv(v).script.Name == 'Utilities' and debug.getinfo(v).name == 'timeFormat' then
+			timeFormat = v
+		end
 	end
 end
 
