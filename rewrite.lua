d9b453ba79befc89ded10a3fc2cdbea98d7d3059
yuiz <solibex.contact@gmail.com>
2024-06-15 00:20:37 +0700

    lalala more rewrite

--- rewrite.lua
+++ rewrite.lua
@@ -4,18 +4,18 @@ local texts = {
     no_slide_cd = {Text = 'no slide cooldown', Tooltip = 'remove slide cooldown'};
 
     -- esp
-    item_esp = {Text = 'item esp', Tooltip = ''};
-    mob_esp = {Text = 'mob esp', Tooltip = ''};
+    item_esp = {Text = 'item esp', Tooltip = 'esp for item'};
+    mob_esp = {Text = 'mob esp', Tooltip = 'esp for mobs'};
 }
 
 local CHECK_MARK = '✅'
 local WARNING_MARK = '⚠️'
 local DENIED_MARK = '❌'
 
-local run_service = game:GetService('RunService')
-local players = game:GetService('Players')
-local replicated_storage = game:GetService('ReplicatedStorage')
-local proximity_prompt_service = game:GetService('ProximityPromptService')
+local run_service = cloneref(game:GetService('RunService')) :: RunService
+local players = cloneref(game:GetService('Players')) :: Players
+local replicated_storage = cloneref(game:GetService('ReplicatedStorage')) :: ReplicatedStorage
+local proximity_prompt_service = cloneref(game:GetService('ProximityPromptService')) :: ProximityPromptService
 
 local modules = replicated_storage:WaitForChild('Modules')
 
@@ -27,6 +27,7 @@ local storages = workspace:WaitForChild('Storages')
 local mobs = workspace:WaitForChild('Mobs')
 
 local client_table, ult_table
+local connections_table = {}
 local npc_shops = {}
 
 local executor = (identifyexecutor and identifyexecutor()) or "shit executor"
@@ -42,6 +43,7 @@ end
 
 local repo = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
 
+local Sense = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Sirius/request/library/sense/source.lua'))()
 local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
@@ -88,6 +90,15 @@ if not client_table then
     texts.no_slide_cd.Tooltip = `[ {WARNING_MARK} ] This feature wont work due to client not being able to hook.`
 end
 
+for name, value in pairs(shop_lib) do
+	if rawget(value, 'ToolTypes') then -- sellers
+		continue
+	end
+
+	table.insert(npc_shops, name)
+end
+
+
 local window = Library:CreateWindow({
 	Title = 'storage ware',
 	Center = true,
@@ -95,6 +106,8 @@ local window = Library:CreateWindow({
 	TabPadding = 8,
 	MenuFadeTime = 0.2
 })
+
+Library:SetWatermark('storage ware rewrite')
 
 local tabs = {
 	main = window:AddTab('Main');
@@ -104,6 +117,11 @@ local tabs = {
 
 local stuff_box = tabs.main:AddLeftGroupbox('stuff')
 local esp_box = tabs.main:AddRightGroupbox('esp')
+
+local player_tab = esp_box:AddTab('player')
+local item_tab = esp_box:AddTab('item')
+local mob_tab = esp_box:AddTab('mob')
+local storage_tab = esp_box:AddTab('storage')
 
 stuff_box:AddToggle('instant_prompt', {
 	Text = texts.instant_prompt.Text,
@@ -120,15 +138,50 @@ stuff_box:AddToggle('no_slide_cd', {
 
 	Callback = function() end
 })
-proximity_prompt_service.PromptButtonHoldBegan:Connect(function(prompt)
+
+esp_box:AddToggle('emen')
+
+local ui_box = tabs.ui_settings:AddLeftGroupbox('ui')
+
+ui_box:AddToggle('watermark_visible', {
+	Text = 'watermark visibilty',
+	Default = false, 
+	Tooltip = 'self-explainatory', 
+
+	Callback = function(value)
+        Library:SetWatermarkVisibility(value)
+    end
+})
+
+ui_box:AddLabel('menu bind'):AddKeyPicker('MenuKeybind', { Default = 'End', NoUI = true, Text = 'Menu keybind' })
+Library.ToggleKeybind = Options.MenuKeybind -- Allows you to have a custom keybind for the menu
+
+ui_box:AddButton({
+    Text = 'unload',
+    Func = Library.Unload,
+    DoubleClick = false,
+    Tooltip = 'unload the whole script'
+})
+
+table.insert(connections_table, proximity_prompt_service.PromptButtonHoldBegan:Connect(function(prompt)
     if Toggles.instant_prompt.Value then
         prompt.HoldDuration = 0
     end
-end)
-run_service.RenderStepped:Connect(function(delta)
+end))
+
+table.insert(connections_table, run_service.RenderStepped:Connect(function(delta)
     if Toggles.no_slide_cd.Value and client_table then
+        -- metatable is an option but some exploits doesnt support it
         rawset(client_table, 'SlideCD', -1)
     end
+end))
+
+Library:OnUnload(function()
+    for _, connection in connections_table do
+        connection:Disconnect()
+    end
+    print('Unloaded!')
+    Library.Unloaded = true
 end)
 
-Library:SetWatermark('storage ware rewrite')
+Sense.Load()
